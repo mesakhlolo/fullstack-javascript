bcb3d469b81535542eb46f7974392b748e63cc05
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2024-11-21 21:26:48 +0700

    eps-3: integrate Axios for fetching posts and add CORS support in SERVER

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+
 function App() {
-  return <div>Hello React with Vite</div>;
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/posts").then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      {listOfPosts.map((post, key) => (
+        <div className="post" key={key}>
+          <div className="title">{post.title}</div>
+          <div className="body">{post.postText}</div>
+          <div className="footer">{post.username}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
