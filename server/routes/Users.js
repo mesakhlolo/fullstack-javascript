a2377de992b3ed0af1d168ebd2f2a1b34e3ebfb9
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2024-11-29 13:36:06 +0700

    feat: add user authentication with registration and login routes

---
+++ server/routes/Users.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) return res.json({ error: "User Doesn't Exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match)
+      return res.json({ error: "Wrong username and password combination" });
+
+    res.json("You have successfully logged in");
+  });
+});
+
+module.exports = router;
